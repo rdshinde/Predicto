08fad6c3ed8f0a28a2d0b69c15bb1b1f760b529c
RISHIKESH DNYANDEV SHINDE <rdshinde2000@gmail.com>
2021-06-24 12:02:45 +0530

    feat: Added functionality to bmi calculator.
    Added functionality to bmi calculator on resources page.

--- static/resources.js
+++ static/resources.js
@@ -1,2 +1,11 @@
-var s = document.bmi.weight.name;
-console.log(s);
+var weight = document.querySelector('#weight');
+var height = document.querySelector('#height');
+var bmi = document.querySelector("#BMI");
+var submit = document.querySelector('#submit');
+
+function submitHandler(){
+    var heightValue = (parseInt(height.value)) / 100 ;
+    var bmiValue = parseInt(weight.value)/ heightValue / heightValue ;
+    bmi.placeholder = Math.round(bmiValue * 100) / 100;
+}
+submit.addEventListener("click",submitHandler);
